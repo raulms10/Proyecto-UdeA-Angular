da826eaa9a143f3d9b5a5a7a97d5aca79f8f6d5f
raulms10 <rantonio.martinez@udea.edu.co>
2019-10-08 10:43:56 -0500

    Algunas pruebas unitarias y el documento

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,17 +1,56 @@
 import { AppPage } from './app.po';
 import { browser, logging } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('Pruebas aplicacion heroes', () => {
+  // Option1
+  // beforeAll(() => browser.get(''));
+  // describe('Initial page', pruebasFuncionales);
+  
+  // Option 2
   let page: AppPage;
 
   beforeEach(() => {
     page = new AppPage();
   });
 
-  it('should display welcome message', () => {
+  it('Debe tener el titulo de la pagina igual a ProyectoUdea', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to proyecto-udea!');
+    // expect(page.getTitleText()).toEqual('ProyectoUdea');
+    expect(page.getTituloText()).toEqual('ProyectoUdea');
   });
+
+  it('Debe tener el encabezado igual a Tour de los heroes', () => {
+    page.navigateTo();
+    expect(page.getEncabezadoText()).toEqual('Tour de los heroes');
+    // expect(browser.getTitle()).toEqual('ProyectoUdea');
+  });
+
+  it('Debe cargar componentes de Villanos al dar clic', () => {
+    page.navigateTo();
+    page.darClicVillanosById();
+    // browser.pause();
+    // browser.sleep(5000); // Se detiene por 5 segundos
+    expect(page.obtenerCompVillanos().isDisplayed()).toBeTruthy();
+  });
+  
+  // it('Debe cargar componentes de detalles al dar clic en heroe', () => {
+  //   page.navigateTo();
+  //   let heros = page.obtenerListaHeroes();
+    
+  //   // browser.pause();
+  //   // browser.sleep(5000); // Se detiene por 5 segundos
+  //   expect(page.obtenerCompVillanos().isDisplayed()).toBeTruthy();
+  // });
+
+  // it('debería ser viisible el componente de editar heroe al dar clic en el heroe', () => {
+  //   page.navigateTo();
+  //   page.darClickHeroes();
+  //   let heroes = page.obtenerListaDeHeroes();
+  //   heroes.first().click();
+  //   expect(page.obtenerComponenteDeEdicionHeroes().isDisplayed).toBeTruthy();
+  //   browser.sleep(6000);
+  // });
+ 
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -20,4 +59,11 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     }));
   });
+
+  // function pruebasFuncionales() {
+  //   it('debe tener el titulo de la pagina igual a ProyectoUdea', () => {
+  //     page.navigateTo();
+  //     expect(page.getTitleText()).toEqual('ProyectoUdea');
+  //   });
+  // }
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -5,7 +5,39 @@ export class AppPage {
     return browser.get(browser.baseUrl) as Promise<any>;
   }
 
-  getTitleText() {
+  getTituloText() {
+    return browser.getTitle() as Promise<string>;;
+  }
+
+  getEncabezadoText() {
     return element(by.css('app-root h1')).getText() as Promise<string>;
+  }
+
+  darClicVillanosById() {
+    return element(by.id('irVillano')).click();
+  }
+  
+  obtenerListaHeroes() {
+    return element.all(by.css('app-heroes li')); // Todos los li dentro de app-heroes
+  }
+
+  obtenerCompHeroesDetalle() {
+    return element(by.css('app-lista-villanos'));
+  }
+  
+  obtenerCompVillanos() {
+    return element(by.css('app-lista-villanos'));
+  }
+  
+  obtenerComponenteDeHeroes(){
+    return element(by.css('app-heroes'));
+  }
+ 
+  obtenerListaDeHeroes(){
+    return element.all(by.css('app-heroes li'));
+  }
+  
+  obtenerComponenteDeEdicionHeroes(){
+    return element(by.css('app-hero-detail'));
   }
 }
